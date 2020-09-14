73626e86b0908d64c66b4faea3aa17a7bdb49c2e
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2020-09-13 21:58:03 -0700

    adding css

--- js/add-content.js
+++ js/add-content.js
@@ -1,3 +1,17 @@
 "use strict"
 
-var 
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'No better time than the present!';
+}   else if (hourNow > 12 ) {
+        greeting = 'Lets have lunch too!';
+}   else if (hourNow > 0) {
+        greeting = 'Its happy hour somewhere!';
+}   else {
+        greeting = 'Cheers!';
+}
+    
+document.write('<h3>' + greeting + '</h3>');    
